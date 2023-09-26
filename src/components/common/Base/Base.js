a5a030eeffff46651a3254bb75925b7f575bffaf
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-09-25 19:29:26 -0500

    Start to refine the sidebar toggling

--- src/components/common/Base/Base.js
+++ src/components/common/Base/Base.js
@@ -3,6 +3,10 @@ import UpperBar from '../UpperBar/UpperBar';
 import SideBar from '../SideBar/SideBar';
 
 function Base({children}) {
+    
+    const handleToggleSidebar = () => {
+
+    };
 
     return (
         <div className="App" >

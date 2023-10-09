ed9d0354db3a79fb3e35ff04d4581d46213903f5
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-10-09 16:29:16 -0500

    Basic navigation implemented

--- src/components/common/SideBar/SideBar.js
+++ src/components/common/SideBar/SideBar.js
@@ -10,7 +10,17 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { Home } from '@mui/icons-material';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { redirect } from "react-router-dom";
+import { useContext } from 'react';
+import IdentityContext from '../../../context/identity';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import PersonIcon from '@mui/icons-material/Person';
+import { Typography } from '@mui/material';
+
 
 export default function SideBar({isOpen}) {
   const [state, setState] = React.useState({
@@ -19,7 +29,7 @@ export default function SideBar({isOpen}) {
     // bottom: false,
     // right: false,
   });
-
+  const {identity} = useContext(IdentityContext);
   // setState({ ...state, left: isOpen});
   // NOTE: Maybe use useEffect here?
 
@@ -54,30 +64,182 @@ export default function SideBar({isOpen}) {
       onKeyDown={toggleDrawer(anchor, false)}
       className="findMe"
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+      <List pt={5}>
+        <ListItem key={Math.random} disablePadding alignItems='center'>
+            <Link to="/" fullWidht >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <Home></Home>
+                </ListItemIcon>
+                <ListItemText primary={"Home"} sx={{textDecoration:"none"}} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
       </List>
+
       <Divider />
+
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          sx={{mt:10}}>
+            <Link to="/register-sale" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Perfil de Usuario"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          sx={{mt:3}}>
+            <Link to="/register-sale" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PointOfSaleIcon></PointOfSaleIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Registrar Compra"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem key={Math.random} disablePadding alignItems='center'sx={{mt:3}}>
+            <Link to="/register-customer" fullWidht>
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <AppRegistrationIcon></AppRegistrationIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Registrar Cliente"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        {
+          (identity.manager) &&
+            <ListItem key={Math.random} disablePadding alignItems='center'sx={{mt:3}}>
+                <Link to="/track-employees" >
+                  <ListItemButton
+                    
+                  >
+                    <ListItemIcon >
+                      <Home></Home>
+                    </ListItemIcon>
+                    <ListItemText primary={"Progreso Mensual E Historia"} />
+                  </ListItemButton>
+                </Link>
+            </ListItem>
+        }
+      </List>
+
+      <Divider />
+      <Typography sx={{mt:3}}>
+        Administracion
+      </Typography>
+      <List sx={{mt:5}}>
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          >
+            <Link to="/crud-customers" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Clientes"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          >
+            <Link to="/crud-sales" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Ventas"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          >
+            <Link to="/crud-products" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"productos"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          >
+            <Link to="/crud-employees" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Empleados"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+        <ListItem
+          key={Math.random}
+          disablePadding
+          alignItems='center'
+          >
+            <Link to="/crud-users" >
+              <ListItemButton
+                
+              >
+                <ListItemIcon >
+                  <PersonIcon></PersonIcon>
+                </ListItemIcon>
+                <ListItemText primary={"Usuarios"} />
+              </ListItemButton>
+            </Link>
+        </ListItem>
+
+
+
+
+
       </List>
     </Box>
   );

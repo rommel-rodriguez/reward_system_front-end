2c90cf3b353291b0f72d0bc6c67fcf34cf3a9336
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-11-06 09:15:59 -0500

    Added basic redux store

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import authService from "../services/authService";
+
+const identitySlice = createSlice({
+    name: 'identity',
+    initialState:
+        JSON.parse(localStorage.getItem('persistentIdentity')) || {},
+    reducers: {
+        async login(state, action) {
+            console.log("Inside Redux Login");
+            try{
+                await authService.login(
+                    action.payload.username,
+                    action.payload.password
+                );
+                const retrievedIdentity =await authService.getIdentity();
+                console.log("Temp Identity: ", retrievedIdentity);
+                // Using Immer, so I can just assign(?) and the identity
+                // application state is updated.
+                state = retrievedIdentity;
+            } catch (error) {
+                console.log(error);
+                throw error;
+            }
+        },
+    },
+});
+
+const store = configureStore({
+    reducer: {
+        identity: identitySlice.reducer,
+    }
+})

2d34b260d5b52dfd7f614180ccec7ff38b6e5c99
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-12-08 12:00:22 -0500

    The modal is almost complete, just needs more styling
    Added conditional rendering for modal. Also, added the feature to hide
    the overflow while the modal is being shown, and restore to normalcy
    when the modal is un-mounted.

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,29 +1,33 @@
 import { Button } from '@mui/material';
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
-function Modal({children, actionBar}) {
+function Modal({children, closeModal, actionBar}) {
+    useEffect(
+        () => {
+            document.body.classList.add('overflow-hidden');
+            return (
+                () => {
+                    document.body.classList.remove('overflow-hidden');
+                }
+            );
+        },
+        []
+    );
+
     return ReactDOM.createPortal (
         <div>
             <div
-                className="absolute inset-0 bg-gray-300 opacity-80"
-                // onClick={closeModal}
+                className="fixed inset-0 bg-gray-300 opacity-80"
+                onClick={closeModal}
                 ></div>
-            <div className="absolute inset-40 p-10 bg-white items-center space-x-1.5 space-y-10 rounded h-80">
-                {children}
-                <Button
-                // startIcon={<OpenWith />}
-                variant="contained"
-                color="error"
-                >
-                   Accept 
-                </Button>
-                <Button
-                // startIcon={<OpenWith />}
-                variant="contained"
-                color="secondary"
-                >
-                   Reject 
-                </Button>
+            <div className="fixed inset-40 p-10 bg-white rounded h-80">
+                <div className='flex flex-col justify-between h-full'>
+                    {children}
+                    <div className='flex justify-end'>
+                        {actionBar}
+                    </div>
+                </div>
             </div>
         </div>,
         document.querySelector('.modal-container')
--- src/components/pages/ModalTest/ModalPage.js
+++ src/components/pages/ModalTest/ModalPage.js
@@ -13,13 +13,83 @@ function ModalPage() {
     const handleCloseModal = () => {
         setShowModal(false);
     };
+    
+    const actionBar = <div className="flex space-x-1.5">
+        <Button  onClick={handleCloseModal} variant="outlined" size="large">Accept</Button>
+        <Button variant="outlined" size="large">Reject</Button>
 
-    const modal = <Modal>
+    </div>;
+
+    const modal = <Modal actionBar={actionBar} closeModal={handleCloseModal}>
         <p>Some text</p>
     </Modal>;
 
     return (
         <Fragment>
+            {
+                showModal && modal 
+            }
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque leo sit amet imperdiet imperdiet. In a nisi ac lectus hendrerit auctor. Etiam tempus commodo lacus in dignissim. Nunc feugiat orci quis tellus egestas, ut dapibus risus vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse condimentum purus in nunc fermentum sollicitudin. Donec sit amet cursus dolor, vitae lobortis nisl. Aliquam erat volutpat. Proin quis tortor bibendum diam tempor vulputate a ac nunc. Sed iaculis, dolor imperdiet semper blandit, felis metus vehicula ligula, non semper justo massa eget arcu. Fusce luctus ante eu sollicitudin rhoncus. Sed feugiat sit amet arcu non dictum. Pellentesque lorem diam, sollicitudin sit amet rutrum eu, fermentum vitae ex. Duis leo augue, posuere in ullamcorper in, consequat quis tellus.
+
+                Sed sodales quam sed egestas faucibus. Donec sed lobortis orci. Duis gravida sem turpis, faucibus convallis metus egestas eu. In efficitur turpis semper, pellentesque sem in, vestibulum ligula. Vivamus id justo mauris. Maecenas porttitor quam lacinia, elementum dolor eget, euismod lorem. Sed efficitur id elit quis tincidunt. Cras euismod leo a est mattis laoreet. Phasellus non ornare lectus. Fusce in porttitor lectus. Proin vel aliquam nibh. Aliquam ac sapien tellus. Praesent condimentum ex orci, a consectetur lectus vehicula ut. Sed pharetra elementum eros eu cursus.
+
+                Etiam pharetra felis at gravida varius. Morbi id elit imperdiet dui venenatis consectetur. Fusce bibendum aliquet volutpat. Praesent hendrerit auctor dui vitae volutpat. Nunc mollis eget velit ut pulvinar. Fusce tempor dignissim neque eget efficitur. Quisque purus dui, condimentum nec dignissim nec, mattis ac turpis. Pellentesque ut metus auctor, hendrerit magna ac, venenatis purus.
+
+                Fusce euismod erat et rutrum ornare. Duis ultricies odio ac aliquet semper. Nullam a posuere justo, quis gravida nisl. Etiam eu malesuada enim. Pellentesque ex nulla, bibendum suscipit finibus at, consequat nec odio. Suspendisse ac tempus libero, vel auctor dui. Ut dignissim auctor diam, in posuere tortor pulvinar non. Vestibulum volutpat neque felis, at suscipit enim molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel nulla tempor, interdum libero nec, ultrices sem.
+
+                Ut ornare felis semper, lacinia metus lacinia, aliquam mi. Cras non laoreet odio. Suspendisse imperdiet ante sed purus iaculis ornare at ac erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fringilla aliquet ipsum eget laoreet. Nulla vitae nisl nec tellus convallis posuere. Nullam felis nibh, hendrerit et ligula non, congue pulvinar ante. Donec dignissim mauris in erat ultrices, ac pretium turpis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut diam mi, sollicitudin vitae dignissim ac, ornare a augue.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque leo sit amet imperdiet imperdiet. In a nisi ac lectus hendrerit auctor. Etiam tempus commodo lacus in dignissim. Nunc feugiat orci quis tellus egestas, ut dapibus risus vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse condimentum purus in nunc fermentum sollicitudin. Donec sit amet cursus dolor, vitae lobortis nisl. Aliquam erat volutpat. Proin quis tortor bibendum diam tempor vulputate a ac nunc. Sed iaculis, dolor imperdiet semper blandit, felis metus vehicula ligula, non semper justo massa eget arcu. Fusce luctus ante eu sollicitudin rhoncus. Sed feugiat sit amet arcu non dictum. Pellentesque lorem diam, sollicitudin sit amet rutrum eu, fermentum vitae ex. Duis leo augue, posuere in ullamcorper in, consequat quis tellus.
+
+                Sed sodales quam sed egestas faucibus. Donec sed lobortis orci. Duis gravida sem turpis, faucibus convallis metus egestas eu. In efficitur turpis semper, pellentesque sem in, vestibulum ligula. Vivamus id justo mauris. Maecenas porttitor quam lacinia, elementum dolor eget, euismod lorem. Sed efficitur id elit quis tincidunt. Cras euismod leo a est mattis laoreet. Phasellus non ornare lectus. Fusce in porttitor lectus. Proin vel aliquam nibh. Aliquam ac sapien tellus. Praesent condimentum ex orci, a consectetur lectus vehicula ut. Sed pharetra elementum eros eu cursus.
+
+                Etiam pharetra felis at gravida varius. Morbi id elit imperdiet dui venenatis consectetur. Fusce bibendum aliquet volutpat. Praesent hendrerit auctor dui vitae volutpat. Nunc mollis eget velit ut pulvinar. Fusce tempor dignissim neque eget efficitur. Quisque purus dui, condimentum nec dignissim nec, mattis ac turpis. Pellentesque ut metus auctor, hendrerit magna ac, venenatis purus.
+
+                Fusce euismod erat et rutrum ornare. Duis ultricies odio ac aliquet semper. Nullam a posuere justo, quis gravida nisl. Etiam eu malesuada enim. Pellentesque ex nulla, bibendum suscipit finibus at, consequat nec odio. Suspendisse ac tempus libero, vel auctor dui. Ut dignissim auctor diam, in posuere tortor pulvinar non. Vestibulum volutpat neque felis, at suscipit enim molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel nulla tempor, interdum libero nec, ultrices sem.
+
+                Ut ornare felis semper, lacinia metus lacinia, aliquam mi. Cras non laoreet odio. Suspendisse imperdiet ante sed purus iaculis ornare at ac erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fringilla aliquet ipsum eget laoreet. Nulla vitae nisl nec tellus convallis posuere. Nullam felis nibh, hendrerit et ligula non, congue pulvinar ante. Donec dignissim mauris in erat ultrices, ac pretium turpis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut diam mi, sollicitudin vitae dignissim ac, ornare a augue.
+            </p>
+
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque leo sit amet imperdiet imperdiet. In a nisi ac lectus hendrerit auctor. Etiam tempus commodo lacus in dignissim. Nunc feugiat orci quis tellus egestas, ut dapibus risus vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse condimentum purus in nunc fermentum sollicitudin. Donec sit amet cursus dolor, vitae lobortis nisl. Aliquam erat volutpat. Proin quis tortor bibendum diam tempor vulputate a ac nunc. Sed iaculis, dolor imperdiet semper blandit, felis metus vehicula ligula, non semper justo massa eget arcu. Fusce luctus ante eu sollicitudin rhoncus. Sed feugiat sit amet arcu non dictum. Pellentesque lorem diam, sollicitudin sit amet rutrum eu, fermentum vitae ex. Duis leo augue, posuere in ullamcorper in, consequat quis tellus.
+
+                Sed sodales quam sed egestas faucibus. Donec sed lobortis orci. Duis gravida sem turpis, faucibus convallis metus egestas eu. In efficitur turpis semper, pellentesque sem in, vestibulum ligula. Vivamus id justo mauris. Maecenas porttitor quam lacinia, elementum dolor eget, euismod lorem. Sed efficitur id elit quis tincidunt. Cras euismod leo a est mattis laoreet. Phasellus non ornare lectus. Fusce in porttitor lectus. Proin vel aliquam nibh. Aliquam ac sapien tellus. Praesent condimentum ex orci, a consectetur lectus vehicula ut. Sed pharetra elementum eros eu cursus.
+
+                Etiam pharetra felis at gravida varius. Morbi id elit imperdiet dui venenatis consectetur. Fusce bibendum aliquet volutpat. Praesent hendrerit auctor dui vitae volutpat. Nunc mollis eget velit ut pulvinar. Fusce tempor dignissim neque eget efficitur. Quisque purus dui, condimentum nec dignissim nec, mattis ac turpis. Pellentesque ut metus auctor, hendrerit magna ac, venenatis purus.
+
+                Fusce euismod erat et rutrum ornare. Duis ultricies odio ac aliquet semper. Nullam a posuere justo, quis gravida nisl. Etiam eu malesuada enim. Pellentesque ex nulla, bibendum suscipit finibus at, consequat nec odio. Suspendisse ac tempus libero, vel auctor dui. Ut dignissim auctor diam, in posuere tortor pulvinar non. Vestibulum volutpat neque felis, at suscipit enim molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel nulla tempor, interdum libero nec, ultrices sem.
+
+                Ut ornare felis semper, lacinia metus lacinia, aliquam mi. Cras non laoreet odio. Suspendisse imperdiet ante sed purus iaculis ornare at ac erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fringilla aliquet ipsum eget laoreet. Nulla vitae nisl nec tellus convallis posuere. Nullam felis nibh, hendrerit et ligula non, congue pulvinar ante. Donec dignissim mauris in erat ultrices, ac pretium turpis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut diam mi, sollicitudin vitae dignissim ac, ornare a augue.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque leo sit amet imperdiet imperdiet. In a nisi ac lectus hendrerit auctor. Etiam tempus commodo lacus in dignissim. Nunc feugiat orci quis tellus egestas, ut dapibus risus vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse condimentum purus in nunc fermentum sollicitudin. Donec sit amet cursus dolor, vitae lobortis nisl. Aliquam erat volutpat. Proin quis tortor bibendum diam tempor vulputate a ac nunc. Sed iaculis, dolor imperdiet semper blandit, felis metus vehicula ligula, non semper justo massa eget arcu. Fusce luctus ante eu sollicitudin rhoncus. Sed feugiat sit amet arcu non dictum. Pellentesque lorem diam, sollicitudin sit amet rutrum eu, fermentum vitae ex. Duis leo augue, posuere in ullamcorper in, consequat quis tellus.
+
+                Sed sodales quam sed egestas faucibus. Donec sed lobortis orci. Duis gravida sem turpis, faucibus convallis metus egestas eu. In efficitur turpis semper, pellentesque sem in, vestibulum ligula. Vivamus id justo mauris. Maecenas porttitor quam lacinia, elementum dolor eget, euismod lorem. Sed efficitur id elit quis tincidunt. Cras euismod leo a est mattis laoreet. Phasellus non ornare lectus. Fusce in porttitor lectus. Proin vel aliquam nibh. Aliquam ac sapien tellus. Praesent condimentum ex orci, a consectetur lectus vehicula ut. Sed pharetra elementum eros eu cursus.
+
+                Etiam pharetra felis at gravida varius. Morbi id elit imperdiet dui venenatis consectetur. Fusce bibendum aliquet volutpat. Praesent hendrerit auctor dui vitae volutpat. Nunc mollis eget velit ut pulvinar. Fusce tempor dignissim neque eget efficitur. Quisque purus dui, condimentum nec dignissim nec, mattis ac turpis. Pellentesque ut metus auctor, hendrerit magna ac, venenatis purus.
+
+                Fusce euismod erat et rutrum ornare. Duis ultricies odio ac aliquet semper. Nullam a posuere justo, quis gravida nisl. Etiam eu malesuada enim. Pellentesque ex nulla, bibendum suscipit finibus at, consequat nec odio. Suspendisse ac tempus libero, vel auctor dui. Ut dignissim auctor diam, in posuere tortor pulvinar non. Vestibulum volutpat neque felis, at suscipit enim molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel nulla tempor, interdum libero nec, ultrices sem.
+
+                Ut ornare felis semper, lacinia metus lacinia, aliquam mi. Cras non laoreet odio. Suspendisse imperdiet ante sed purus iaculis ornare at ac erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fringilla aliquet ipsum eget laoreet. Nulla vitae nisl nec tellus convallis posuere. Nullam felis nibh, hendrerit et ligula non, congue pulvinar ante. Donec dignissim mauris in erat ultrices, ac pretium turpis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut diam mi, sollicitudin vitae dignissim ac, ornare a augue.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque leo sit amet imperdiet imperdiet. In a nisi ac lectus hendrerit auctor. Etiam tempus commodo lacus in dignissim. Nunc feugiat orci quis tellus egestas, ut dapibus risus vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse condimentum purus in nunc fermentum sollicitudin. Donec sit amet cursus dolor, vitae lobortis nisl. Aliquam erat volutpat. Proin quis tortor bibendum diam tempor vulputate a ac nunc. Sed iaculis, dolor imperdiet semper blandit, felis metus vehicula ligula, non semper justo massa eget arcu. Fusce luctus ante eu sollicitudin rhoncus. Sed feugiat sit amet arcu non dictum. Pellentesque lorem diam, sollicitudin sit amet rutrum eu, fermentum vitae ex. Duis leo augue, posuere in ullamcorper in, consequat quis tellus.
+
+                Sed sodales quam sed egestas faucibus. Donec sed lobortis orci. Duis gravida sem turpis, faucibus convallis metus egestas eu. In efficitur turpis semper, pellentesque sem in, vestibulum ligula. Vivamus id justo mauris. Maecenas porttitor quam lacinia, elementum dolor eget, euismod lorem. Sed efficitur id elit quis tincidunt. Cras euismod leo a est mattis laoreet. Phasellus non ornare lectus. Fusce in porttitor lectus. Proin vel aliquam nibh. Aliquam ac sapien tellus. Praesent condimentum ex orci, a consectetur lectus vehicula ut. Sed pharetra elementum eros eu cursus.
+
+                Etiam pharetra felis at gravida varius. Morbi id elit imperdiet dui venenatis consectetur. Fusce bibendum aliquet volutpat. Praesent hendrerit auctor dui vitae volutpat. Nunc mollis eget velit ut pulvinar. Fusce tempor dignissim neque eget efficitur. Quisque purus dui, condimentum nec dignissim nec, mattis ac turpis. Pellentesque ut metus auctor, hendrerit magna ac, venenatis purus.
+
+                Fusce euismod erat et rutrum ornare. Duis ultricies odio ac aliquet semper. Nullam a posuere justo, quis gravida nisl. Etiam eu malesuada enim. Pellentesque ex nulla, bibendum suscipit finibus at, consequat nec odio. Suspendisse ac tempus libero, vel auctor dui. Ut dignissim auctor diam, in posuere tortor pulvinar non. Vestibulum volutpat neque felis, at suscipit enim molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel nulla tempor, interdum libero nec, ultrices sem.
+
+                Ut ornare felis semper, lacinia metus lacinia, aliquam mi. Cras non laoreet odio. Suspendisse imperdiet ante sed purus iaculis ornare at ac erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fringilla aliquet ipsum eget laoreet. Nulla vitae nisl nec tellus convallis posuere. Nullam felis nibh, hendrerit et ligula non, congue pulvinar ante. Donec dignissim mauris in erat ultrices, ac pretium turpis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut diam mi, sollicitudin vitae dignissim ac, ornare a augue.
+            </p>
+
             <Button
              startIcon={<OpenWith />}
              onClick={handleClick}
@@ -28,9 +98,6 @@ function ModalPage() {
              >
                 Open Modal
             </Button>
-            {
-                showModal && modal 
-            }
 
         </Fragment>
     );
